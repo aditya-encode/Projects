5acf5345e0787b7ed6ec86b349b10d4778f3bea0
Aditya <aditya@gmail.com>
2026-09-25 13:21:30 +0530

    learned the difference between httpresponse & render and practiced them and also created template to separate the designing file and logic file

--- core/home/views.py
+++ core/home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to my django learning Journey.")
+    return render(request,"index.html")
 
