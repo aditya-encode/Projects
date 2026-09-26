cd90b6e49464066dac18afa236023f395f5d5fef
Aditya <aditya@gmail.com>
2026-09-26 16:09:58 +0530

    made one table with column like s.no,name,age and voting right and made used if codition and forloop to print yes/no according to the age of a person using 'django template language'

--- core/home/views.py
+++ core/home/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,"index.html")
+    peoples=[{'name':'Aditya',"age":24},
+             {'name':'Het',"age":23},
+             {'name':'Bhautik',"age":15}
+             ]
+    return render(request,"index.html",context={'peoples':peoples})
 
